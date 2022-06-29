b8c4153a6b116754b74fe4abfbb807260f2c81fb
VarsharaneeAR <100801262+VarsharaneeAR@users.noreply.github.com>
2022-06-30 00:44:32 +0400

    removed doubled formatDay

--- src/app.js
+++ src/app.js
@@ -1,68 +1,71 @@
 
-function formatDate(timestamp)
-{
-let date = new Date(timestamp);
-let hours = date.getHours();
-if( hours<10){
-    hours= `0${hours}`
-}
-let minutes = date.getMinutes();
-if (minutes <10){
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
     minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`;
-}
-  
-function formatDate(timestamp){
-  let date = new Date (timestamp * 1000);
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [ "Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
 
-function formatDay (timestamp){
-let date = new Date(timestamp*1000);
-let day = date.getDay()
-let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weather-forecast");
 
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
 
-return days[day];
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
-
-function displayForecast(response){
-    let forecast = response.data.daily; 
-    let forecastElement = document.querySelector("#weather-forecast");
-   
-
-    let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) 
-    { if (index < 6) {
-        forecastHTML =
-          forecastHTML +
-          `
-         <div class="col-md-2"> 
-            <div class="weather-forecast-date">
-              ${formatDay(forecastDay.dt)}
-            </div>
-            <img
-            src="http://openweathermap.org/img/wn/${
-                    forecastDay.weather[0].icon
-                  }@2x.png"
-            alt=""
-            width="42"
-          />
-              <span class="weather-forecast-temp-max"> ${Math.round(forecastDay.temp.max)}°</span>|<span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span> 
-            </div>
-            </div>`;
-        }});
-
-
-        forecastHTML = forecastHTML +`</div>`;
-        forecastElement.innerHTML= forecastHTML;
-      }
 
 function getForecast(coordinates)
 { console.log(coordinates);
@@ -109,7 +112,6 @@ search(cityInputElement.value)
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-
 function showCelsiusTemp(event){
     event.preventDefault();
     let tempElement = document.querySelector("#temperature");
@@ -118,7 +120,6 @@ function showCelsiusTemp(event){
     fahLink.classList.remove("active");
 }
 let celsiusTemperature = null;
-
 
 
 let celLink = document.querySelector("#cel-link");
